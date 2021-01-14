ae28d36cd56cf77217ac6ac28469b6073934d05f
Marcelo Tamashiro <marcelo.s.tamashiro@gmail.com>
2021-01-14 16:53:44 -0300

    criando modelo de subcoleção produtos

--- api/banco-de-dados/criarTabelas.js
+++ api/banco-de-dados/criarTabelas.js
@@ -1,5 +1,13 @@
-const ModeloTabela = require('../rotas/fornecedores/modeloTabelaFornecedor');
+const modelos = [
+    require('../rotas/fornecedores/modeloTabelaFornecedor'),
+    require('../rotas/fornecedores/produtos/ModeloTabelaProduto')
+]
 
-ModeloTabela
-    .sync()
-    .then(() => console.log('tabela fornecedor criada com sucesso'));
+async function criarTabelas(){
+    for(let contador = 0; contador < modelos.length; contador++){
+        const modelo = modelos[contador]
+        await modelo.sync();
+    }
+}
+
+criarTabelas()
--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -71,4 +71,7 @@ roteador.delete('/:idFornecedor', async(requisicao, resposta, proximo) => {
     }
 });
 
+const roteadorProdutos = require('./produtos');
+roteador.use('/:idFornecedor/produtos', roteadorProdutos);
+
 module.exports = roteador;
