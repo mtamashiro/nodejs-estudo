29b743b28f349361a499fe2e689850df7911862c
Marcelo Tamashiro <marcelo.s.tamashiro@gmail.com>
2021-01-20 09:42:08 -0300

    adicionando metodo post e delete de produtos

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -72,6 +72,19 @@ roteador.delete('/:idFornecedor', async(requisicao, resposta, proximo) => {
 });
 
 const roteadorProdutos = require('./produtos');
-roteador.use('/:idFornecedor/produtos', roteadorProdutos);
+
+const verificarFornecedor = async (req, resp, proximo) => {
+    try{
+        const id = req.params.idFornecedor;
+        const fornecedor = new Fornecedor({id: id});
+        await fornecedor.carregar();
+        req.fornecedor = fornecedor;
+        proximo()
+    }catch (erro){
+        proximo(erro);
+    }
+}
+
+roteador.use('/:idFornecedor/produtos', verificarFornecedor, roteadorProdutos);
 
 module.exports = roteador;
